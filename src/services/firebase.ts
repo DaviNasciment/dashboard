beae92d856a0565e1e7ba710a60d3caf307a3f14
Davi Nascimento <davinascimentto03@gmail.com>
2022-06-17 15:17:35 -0300

    Env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_APIKEY_SECRET,
-    authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN_SECRET,
-    projectId: process.env.NEXT_PUBLIC_PROJECTID_SECRET,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET_SECRET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDID_SECRET,
-    appId: process.env.NEXT_PUBLIC_APPID_SECRET,
+    apiKey: process.env.APIKEY_SECRET,
+    authDomain: process.env.AUTHDOMAIN_SECRET,
+    projectId: process.env.PROJECTID_SECRET,
+    storageBucket: process.env.STORAGEBUCKET_SECRET,
+    messagingSenderId: process.env.MESSAGINGSENDID_SECRET,
+    appId: process.env.APPID_SECRET,
     measurementId: process.env.MEASUREMENTID_SECRET,
 };
 
